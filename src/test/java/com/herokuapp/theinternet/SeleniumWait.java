5d7e1024dd389414a09231177bc045508c13e3c1
Iffat Ahmed Khan <iffat.ahmedk@gmail.com>
2020-10-13 21:16:07 +0500

    dynamic loading 1 and 2

--- src/test/java/com/herokuapp/theinternet/SeleniumWait.java
+++ src/test/java/com/herokuapp/theinternet/SeleniumWait.java
@@ -4,9 +4,11 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,26 +21,67 @@ import org.testng.annotations.Test;
 
 public class SeleniumWait {
 
-	String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+	//String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
 	static WebDriver driver;
 	
-	@Test
+	@Test (enabled=false)
 	public void waitForHelloWorld() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		driver = new ChromeDriver();
 		
-		driver.get(url);
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
 		driver.manage().window().maximize();
 		
 		WebElement startButton = driver.findElement(By.xpath("//div[@id='start']/button"));
 		startButton.click();
 
-		WebElement finishText = driver.findElement(By.id("finish"));
+		WebElement finishText = driver.findElement(By.cssSelector("#finish > h4"));
 		
         WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOf(finishText));
+        try {
+			wait.until(ExpectedConditions.visibilityOf(finishText));
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Exception catched" + e.getMessage());
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+        String text = finishText.getText();
         
-		Assert.assertTrue(finishText.getText().contains("Hello World"), "Hello World not found");
+		Assert.assertTrue(text.contains("Hello World"), "Finish Text:" + text);
+		
+		driver.quit();
+	}
+
+	@Test
+	public void waitForHelloWorld2() {
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+		driver.manage().window().maximize();
+		
+		WebElement startButton = driver.findElement(By.xpath("//div[@id='start']/button"));
+		startButton.click();
+
+		//WebElement finishText = driver.findElement(By.cssSelector("#finish > h4"));
+		
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        try {
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("finish")));
+			WebElement finishText = driver.findElement(By.cssSelector("#finish > h4"));
+			String text = finishText.getText();
+	        
+			Assert.assertTrue(text.contains("Hello World"), "Finish Text:" + text);
+			
+			} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Exception catched" + e.getMessage());
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+        
+        //String text = finishText.getText();
+        
+		//Assert.assertTrue(text.contains("Hello World"), "Finish Text:" + text);
 		
 		driver.quit();
 	}
